f7f01090a588f9826610d00032deaa5c29385069
Jiri Kolafa <jiri.kolafa@vscht.cz>
2024-02-19 21:49:08 +0100

    first+4

--- sim/intrapot.c
+++ sim/intrapot.c
@@ -1,5 +1,5 @@
-/* 
-  this module contains intramolecular (bonded) potential terms 
+/*
+  this module contains intramolecular (bonded) potential terms
   2020: Morse added (expansion to r^4 only, use #define MORSE)
   2016: bug in pressure tensor for Urey-Bradley fixed
   2008: pressure tensor support added
@@ -13,10 +13,10 @@
 
 // #define DUMP
 
-#ifdef COOK 
+#ifndef BLEND
 #  include "simglob.h"
 #  include "units.h"
-#endif /*# COOK  */
+#endif /*# BLEND */
 
 #if PRESSURETENSOR&PT_VIR
 #  if PRESSURETENSOR&PT_OFF
@@ -61,14 +61,14 @@ double bondpot( /*************************************************** bondpot */
 
 /********************************************************************
   Bond potential of the form
-  
+
                         2
   U = K (|r0-r1|-length)
-  
+
   for bond    r0--r1   (length=0 allowed)
 
   #ifdef MORSE: expanded Morse potential (length>0 required)
-  
+
   U =  D [ 1-exp(-a x)) ]^2   [original Morse]
     =  K x^2  * [1 - a x + 7/12*(a x)^2 ]
   where x=|r0-r1|-length, K = D a^2
@@ -79,7 +79,7 @@ double bondpot( /*************************************************** bondpot */
   return values
     0 (if measure==0)
     U (if measure==1)
-  
+
   if measure=2 then also phi:=bond length is returned
 ********************************************************************/
 
@@ -100,17 +100,17 @@ double bondpot( /*************************************************** bondpot */
     double x=r-parm->length;
 
     f=parm->Ki2*x/r*(1-parm->fa1*x+parm->fa2*Sqr(x));
-  
+
     f0[0]+=rf=dr[0]*f; f1[0]-=rf;
     f0[1]+=rf=dr[1]*f; f1[1]-=rf;
     f0[2]+=rf=dr[2]*f; f1[2]-=rf;
     /*  VVO(f0,+=dr,*=f) VV(f1,-=dr) */
 
     if (measure) {
-#  ifdef COOK
+#  ifndef BLEND
       En.vir+=f*rr;
       CALCULATEPRESSURETENSOR(dr,f)
-#  endif /*# COOK */
+#  endif /*# BLEND */
       U=Sqr(x)*(parm->K-parm->Ka*x+parm->Ka2*Sqr(x));
       //      prt("%.9g %.9g %.9g MORSE r-r0 U f",x,U,f);
     }
@@ -122,20 +122,20 @@ double bondpot( /*************************************************** bondpot */
         4/2012: optimized + parm->length=r=0 allowed */
     if (parm->length) f=parm->Ki2*(1-parm->length/r);
     else f=parm->Ki2;
-    
+
     f0[0]+=rf=dr[0]*f; f1[0]-=rf;
     f0[1]+=rf=dr[1]*f; f1[1]-=rf;
     f0[2]+=rf=dr[2]*f; f1[2]-=rf;
     /*  VVO(f0,+=dr,*=f) VV(f1,-=dr) */
-  
+
     if (measure) {
-#  ifdef COOK
+#ifndef BLEND
       En.vir+=f*rr;
       CALCULATEPRESSURETENSOR(dr,f)
-#  endif /*# COOK */
+#endif /*# BLEND */
       U=parm->K*Sqr(r-parm->length); }
   }
-  
+
 #ifdef DUMP
   prt("bond: %d-%d  r=%g u=%g  %.7f %.7f %.7f  %.7f %.7f %.7f",
       (vector*)r0-cfg[0]->rp,(vector*)r1-cfg[0]->rp,
@@ -146,7 +146,7 @@ double bondpot( /*************************************************** bondpot */
   return U;
 }
 
-  
+
 double anglepot( /************************************************ anglepot */
   vector r0,vector r1,vector r2,
   vector f0,vector f1,vector f2,
@@ -157,28 +157,28 @@ double anglepot( /************************************************ anglepot */
   angle>0: Bond angle potential of the form
                      2                       2
     U = K (phi-angle)  + Kub (|r0-r2|-length)
-  
+
   angle<0: special form (close to phi=180 the same as above for
            angle=180, but not singular) (added 6/99)
-  
+
     U = 2 K (1+cos phi)
-  
+
   for angle       r1--r2
                   /
                  /  phi
                 r0
-  
+
   #ifdef UREY_BRADLEY (5/99): Kub and length are Urey-Bradley constants
-  
+
   r0, r1, r2 = positions of atoms
   f0, f1, f2 = forces will be summed here
   parm = pointer to parameters
   return values
     0 (if measure==0)
     U (if measure==1)
-  
+
   Notes:
-    The virial (= trace of the pressure tensor) of the angle potential 
+    The virial (= trace of the pressure tensor) of the angle potential
       K (phi-angle)^2 is zero (the individual components are not).
     The virial of the Urey-Bradley term is not zero.
 ********************************************************************/
@@ -220,7 +220,7 @@ double anglepot( /************************************************ anglepot */
   VV(f0,+=grada)
   VVV(f1,-=grada,+gradb)
   VV(f2,+=gradb)
-  
+
 #if PRESSURETENSOR&PT_VIR
   if (measure) {
     CALCULATEPRESSURETENSORG(grada,a)
@@ -235,19 +235,18 @@ double anglepot( /************************************************ anglepot */
     norm=sqrt(aa);
 
     /*.....if (measure==2) phi=r;*/
-   
+
     f=parm->Kubi2*(1-parm->length/norm);
     f0[0]+=df=a[0]*f; f2[0]-=df;
     f0[1]+=df=a[1]*f; f2[1]-=df;
     f0[2]+=df=a[2]*f; f2[2]-=df;
 
     if (measure) {
-      U+=parm->Kub*Sqr(norm-parm->length); 
-
-#  ifdef COOK
+      U+=parm->Kub*Sqr(norm-parm->length);
+#  ifndef BLEND
       En.vir+=f*aa;
       CALCULATEPRESSURETENSOR(a,f)
-#  endif /*# COOK */
+#  endif /*# BLEND */
     }
   }
 #endif /*# UREY_BRADLEY */
@@ -273,7 +272,7 @@ double dihedralpot( /***************************************** dihedralpot */
 
 /********************************************************************
   Torsion (dihedral) potential of the form
-  
+
   n=0:           U=K[0]*(phi-K[1])^2       (K[1]=0 or PI only)
   n={1,2,3,4,6}: U=|K[0]|+K[0]*cos(n*phi)  [1 term]
   n=5:           K>0: U=K*cos^2(phi) for cos(phi)>0
@@ -283,19 +282,19 @@ double dihedralpot( /***************************************** dihedralpot */
                    |n|         i
   n<0:           U=SUM K[i] cos phi        [more terms]
                    i=0
-  
+
   NOTE: U=K*(1+cos(n*phi-PI)) = |K'|+K'*cos(n*phi), where K'=-K
-  
+
   for molecule      r1--r2
   (or part of       /    \
   molecule)       r0      r3
-  
+
   phi is the dihedral angle
-  
+
   return values
     0 (if measure==0)
     U (if measure==1)
-  
+
   Note: virial of the dihedral potential is zero
 ********************************************************************/
 
@@ -310,11 +309,11 @@ double dihedralpot( /***************************************** dihedralpot */
 
   int i,n=parm->n;
 
-  VVV(a,=r1,-r0) 
-  VVV(b,=r2,-r1) 
+  VVV(a,=r1,-r0)
+  VVV(b,=r2,-r1)
   VVV(c,=r3,-r2)
-  ab=SCAL(a,b); 
-  bc=SCAL(b,c); 
+  ab=SCAL(a,b);
+  bc=SCAL(b,c);
   bb=SQR(b);
   abbb=ab/bb; VVV(ap,= -a,+abbb*b)
   bcbb=bc/bb; VVV(cp,=  c,-bcbb*b)
@@ -356,7 +355,7 @@ double dihedralpot( /***************************************** dihedralpot */
 	(int)(phi*(parm->dihhist->grid/PI)),
 	parm->dihhist->grid); */
     parm->dihhist->hist[(int)(phi*parm->dihhist->q)]++;
-    if (phi>PI) phi-=2*PI;    
+    if (phi>PI) phi-=2*PI;
     if (dihcp) fprintf(dihcp," %9.5f",phi*180/PI);
     if (fabs(phi)<parm->dihhist->gauche_trans) parm->dihhist->gauche++;
     else parm->dihhist->trans++; }
@@ -405,9 +404,9 @@ double dihedralpot( /***************************************** dihedralpot */
 #endif /*# BLEND */
 	case 6: U=((32*bb-48)*bb+18)*bb-1; break; }
 
-      U=fabs(K)+K*U; 
+      U=fabs(K)+K*U;
 #ifdef BLEND
-      pureU:; 
+      pureU:;
 #endif /*# BLEND */
       }
 
@@ -442,7 +441,7 @@ double dihedralpot( /***************************************** dihedralpot */
   /* note that vector a=r1-r0 here, while a=r0-r1 in anglepot */
   if (measure) {
     CALCULATEPRESSURETENSORG(-grada,a)
-    CALCULATEPRESSURETENSORG(-gradb,b) 
+    CALCULATEPRESSURETENSORG(-gradb,b)
     CALCULATEPRESSURETENSORG(-gradc,c) }
 #endif /*# PRESSURETENSOR&PT_VIR */
 
@@ -464,23 +463,23 @@ double improperpot( /****************************************** improperpot */
 
 /********************************************************************
   Improper torsion potential of the form
-  
+
   n=0: U=K[0]*(phi-K[1])^2
   n!=0: not implemented
-  
+
                             r2
   for molecule             /
   (or part of       r3---r0
   molecule)                \
                             r1
-  
+
   Where phi is oriented angle of plane r1-r2-r3 from plane r0-r1-r2.
   In the above arrangement of atoms, phi is positive if r0 is below
   the plane r1-r2-r3; in other words, if vectors (r1-r0,r2-r0,r3-r0)
   are arranged as right-hand (x,y,z) Cartesian system.
   The chirality of r0, with respect to atoms r1,r2,r3 (in this order),
   is also positive.
-  
+
   Good for bond angles |phi|<PI/2 (and not too close to PI/2).
   sin(phi) is used to calculate phi and hence phi and PI-phi are not
   distinguished.  This should not matter because the improper torsion potential
@@ -489,16 +488,16 @@ double improperpot( /****************************************** improperpot */
   CHARMM guys -- we do not have problems for phi~0.  cos(phi) is calculated
   for testing purposes and error condition is set if cos(phi)<0.
   ... this has been fixed!
-  
+
   If angle==0 then procedure torsion can be used instead (allowing any phi
   and a bit faster).
-  
+
   return values
     0 (if measure==0)
     U (if measure==1)
   phi also returned if measure>=2
   no U calculated if measure==3
-  
+
   Note: virial of the improper potential is zero
 ********************************************************************/
 
@@ -506,26 +505,26 @@ double improperpot( /****************************************** improperpot */
   vector v01,v12,v13, a,b,A,B, a0,a1,a2,b1,b2,b3;
   double s,t,q,sinphi,aa,bb,norm,ab,x,df;
   double U=0;
-  
+
   VVV(v01,=r1,-r0)
   VVV(v12,=r2,-r1)
   VVV(v13,=r3,-r1)
-  
+
   VECT(a,v01,v12) /* vector a is perpendicular to 012 plane */
-  
+
   q=SQR(v12);
   s=SCAL(v12,v13)/q;
   VVV(b,=v13,-s*v12) /* vector b lies in 123 plane and is perpendicular to 12 */
-  
+
   norm=sqrt( (aa=SQR(a))*(bb=SQR(b)) );
   ab=SCAL(a,b);
   sinphi=ab/norm;
-  
+
   /* to fix improbable numerical things like |sinphi|=1+1e-16 */
   if (sinphi<-1) sinphi=-1;
   if (sinphi>1) sinphi=1;
   df=asin(sinphi);
-  
+
   /* testing */
   VECT(B,v12,v13)
   if (SCAL(B,a)<0) {
@@ -534,41 +533,41 @@ double improperpot( /****************************************** improperpot */
     if (df>0) df=PI-df;
     else df=-PI-df;
     norm=-norm; }
-  
+
   if (measure>=2) {
     phi=df;
     if (measure==3) return 0; }
-  
+
   df-=parm->K[1];
-  
+
   norm=(-2*parm->K[0])/sqrt(1-Sqr(sinphi))*df/norm;
   x=norm*ab/aa; VVV(A,=norm*b,-x*a)
   x=norm*ab/bb; VVV(B,=norm*a,-x*b)
-  
+
   VECT(a0,A,v12)
   VECT(a2,A,v01)
   VVV(a1,=a0,+a2) /* *-1 */
-  
+
   t=SCAL(B,v12)/q;
   x=-2*s*t;
   VVVV(b2,=-s*B,+t*v13,+x*v12)
-  
+
   VVV(b3,=B,-t*v12)
-  
+
   VVV(b1,=b3,+b2) /* *-1 */
-  
+
   if (measure) U=parm->K[0]*Sqr(df);
-  
+
   VV(f0,+=a0)
   VVV(f1,-=a1,+b1)
   VVV(f2,+=a2,+b2)
   VV(f3,+=b3)
 
-#if PRESSURETENSOR&PT_VIR  
+#if PRESSURETENSOR&PT_VIR
   if (measure) {
     CALCULATEPRESSURETENSORG(-a0,v01)
-    CALCULATEPRESSURETENSORG(a2,v12) 
-    CALCULATEPRESSURETENSORG(b2,v12) 
+    CALCULATEPRESSURETENSORG(a2,v12)
+    CALCULATEPRESSURETENSORG(b2,v12)
     CALCULATEPRESSURETENSORG(b3,v13) }
 #endif /*# PRESSURETENSOR&PT_VIR   */
 
